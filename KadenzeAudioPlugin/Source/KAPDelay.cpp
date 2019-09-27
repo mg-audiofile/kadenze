c766b94afc2d166948605072593ab1ff8e157584
mg-audiofile <mgaudiofile.git@gmail.com>
2019-09-26 21:40:37 -0700

    Delay Module

--- KadenzeAudioPlugin/Source/KAPDelay.cpp
+++ KadenzeAudioPlugin/Source/KAPDelay.cpp
@@ -9,3 +9,33 @@
 */
 
 #include "KAPDelay.h"
+
+KAPDelay::KAPDelay()
+{
+
+}
+
+KAPDelay::~KAPDelay()
+{
+
+}
+
+void KAPDelay::setSampleRate(double inSampleRate)
+{
+
+}
+
+void KAPDelay::reset()
+{
+
+}
+
+void KAPDelay::process(float* inAudio,
+	float inTime,
+	float inFeedback,
+	float inWetDry,
+	float* outAudio,
+	int inNumSamplesToRender)
+{
+
+}
