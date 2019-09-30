539f1f0a0c4412c8fda1bc449301c9c9d13f491f
mg-audiofile <mgaudiofile.git@gmail.com>
2019-09-30 15:11:29 -0700

    LFO module

--- KadenzeAudioPlugin/Source/KAPLfo.cpp
+++ KadenzeAudioPlugin/Source/KAPLfo.cpp
@@ -28,10 +28,26 @@ void KAPLfo::reset()
 
 void KAPLfo::setSampleRate(double inSampleRate)
 {
-
+	zeromem(mBuffer, sizeof(float) * maxBufferDelaySize);
 }
 
 void KAPLfo::process(float inRate, float inDepth, int inNumSamples)
 {
+	const float rate = jmap(inRate, 0.0f, 1.0f, 0.01f, 10.0f);
 
+	for (int i = 0; i < inNumSamples; i++) {
+		mPhase = mPhase + (rate / mSampleRate);
+
+		if (mPhase > 1.0f) {
+			mPhase -= 1.0f;
+		}
+
+		const float lfoPosition = sin(mPhase * k2PI) * inDepth;
+		mBuffer[i] = lfoPosition;
+	}
+}
+
+float* KAPLfo::getBuffer()
+{
+	return mBuffer;
 }
--- KadenzeAudioPlugin/Source/KAPLfo.h
+++ KadenzeAudioPlugin/Source/KAPLfo.h
@@ -22,6 +22,8 @@ public:
 	void setSampleRate(double inSampleRate);
 	void process(float inRate, float inDepth, int inNumSamples);
 
+	float* getBuffer();
+
 private:
 
 	double mSampleRate;
